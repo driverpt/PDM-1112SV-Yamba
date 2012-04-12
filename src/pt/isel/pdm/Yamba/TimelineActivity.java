75b2003730da2307f74e4535909cf3e6d4e0e7be
Luís Miguel Dias Duarte <lduarte@luisduarte.net>
2012-04-12 21:17:29 +0000

    Merged with Github repository. Added DetailActivity. Created AbstractClass for Activities with Preferences Enabled

--- src/pt/isel/pdm/Yamba/TimelineActivity.java
+++ src/pt/isel/pdm/Yamba/TimelineActivity.java
@@ -3,7 +3,7 @@ package pt.isel.pdm.Yamba;
 import java.util.ArrayList;
 import java.util.List;
 
-import android.app.Activity;
+import winterwell.jtwitter.Twitter;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
@@ -11,89 +11,109 @@ import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
-import winterwell.jtwitter.Twitter;
+import android.widget.Toast;
 
-public class TimelineActivity extends Activity implements OnClickListener {
+public class TimelineActivity extends PreferencesEnabledActivity implements
+        OnClickListener {
 
-    private List< TwitterStatus > timeline = initTimeline();
-
-    private ListView view;
-    private SimpleAdapter adapter;
-    private Button refreshButton;
+    private List<TwitterStatus> timeline = initTimeline();
+    private boolean            isFirstTime = true;
     
-    private static List< TwitterStatus > initTimeline() {
-        List< TwitterStatus > l = new ArrayList< TwitterStatus >();
+    private ListView            view;
+    private SimpleAdapter       adapter;
+    private Button              refreshButton;
+
+    private static List<TwitterStatus> initTimeline() {
+        List<TwitterStatus> l = new ArrayList<TwitterStatus>();
         return l;
     }
-    
+
     @Override
-    protected void onCreate( Bundle savedInstanceState ) {
-        setContentView( R.layout.timeline );
-        super.onCreate( savedInstanceState );
-        view = ( ListView ) findViewById( android.R.id.list );
-        
-        refreshButton = ( Button ) findViewById( R.id.button1 );
-        refreshButton.setOnClickListener( this );
-        
+    protected void onCreate(Bundle savedInstanceState) {
+        setContentView(R.layout.timeline);
+        super.onCreate(savedInstanceState);
+        view = (ListView) findViewById(android.R.id.list);
+
+        refreshButton = (Button) findViewById(R.id.refreshButton);
+        refreshButton.setOnClickListener(this);
+
         App app = (App) getApplication();
-        if (app.lastRefresh != null && !app.lastRefresh.isEnabled()) {
+        if (app.lastRefresh != null && !app.lastRefresh.isEnabled() ) {
             disableRefresh();
         }
-        
+
         app.lastRefresh = refreshButton;
+
+        String[] from = { TwitterStatus.KEY_USER, TwitterStatus.KEY_DATE,
+                TwitterStatus.KEY_TWEET };
+        int[] to = { R.id.user, R.id.date, R.id.tweet };
+        adapter = new SimpleAdapter(this, timeline, R.layout.timeline_item,
+                from, to);
+        view.setAdapter(adapter);
         
-        String[] from = { TwitterStatus.KEY_USER, TwitterStatus.KEY_DATE, TwitterStatus.KEY_TWEET };
-        int[] to = { R.id.user, R.id.date, R.id.tweet };        
-        adapter = new SimpleAdapter( this, timeline, R.layout.timeline_item, from, to );
-        view.setAdapter( adapter );
-        
-        updateTimeline();
+        if( isFirstTime ) {
+            disableRefresh();
+            updateTimeline();
+            isFirstTime = true;
+        }
     }
-    
-    private class UpdateTimelineTask extends AsyncTask< Void, Void, Void > {
+
+    private class UpdateTimelineTask extends AsyncTask<Void, Void, Void> {
+
+        private volatile Exception exception;
 
         @Override
-        protected Void doInBackground( Void ... params ) {
-            List<Twitter.Status> twitterStatus = ((App) getApplication()).getTwitter().getPublicTimeline();
-            
-            for( Twitter.Status status : twitterStatus ) {
-                TwitterStatus temp = new TwitterStatus( status.getId(), status.getUser().getName(), status.getCreatedAt(), status.getText() );
-                timeline.add( temp );
+        protected Void doInBackground(Void... params) {
+            try {
+                List<Twitter.Status> twitterStatus = ((App) getApplication())
+                        .getTwitter().getPublicTimeline();
+                for (Twitter.Status status : twitterStatus) {
+                    TwitterStatus temp = new TwitterStatus(status.getId(),
+                            status.getUser().getName(), status.getCreatedAt(),
+                            status.getText());
+                    timeline.add(temp);
+                }
+            } catch (Exception e) {
+                exception = e;
             }
             return null;
         }
-        
+
         @Override
-        protected void onPostExecute( Void result ) {
-            adapter.notifyDataSetChanged();
+        protected void onPostExecute(Void result) {
+            if (exception != null) {
+                Toast.makeText(TimelineActivity.this, exception.getMessage(),
+                        Toast.LENGTH_LONG).show();
+            } else {
+                adapter.notifyDataSetChanged();
+            }
             enableRefresh();
         }
-        
+
     }
-    
+
     private void updateTimeline() {
         UpdateTimelineTask task = new UpdateTimelineTask();
         timeline.clear();
         task.execute();
     }
-    
+
     @Override
     protected void onDestroy() {
         super.onDestroy();
     }
 
-    public void onClick( View v ) {
+    public void onClick(View v) {
         disableRefresh();
         updateTimeline();
     }
-    
+
     private void disableRefresh() {
-        refreshButton.setEnabled( false );
+        refreshButton.setEnabled(false);
     }
-    
+
     private void enableRefresh() {
         Button refresh = ((App) getApplication()).lastRefresh;
-        refresh.setEnabled(true);        
-        
-    }    
+        refresh.setEnabled(true);
+    }
 }
