e620f66bf6e20a1cece73297e1f2ba07b5893a78
Luís Miguel Dias Duarte <lduarte@luisduarte.net>
2012-04-12 20:16:27 +0000

    Merged with Git Repository

--- src/pt/isel/pdm/Yamba/App.java
+++ src/pt/isel/pdm/Yamba/App.java
@@ -1,30 +1,32 @@
 package pt.isel.pdm.Yamba;
 
-import winterwell.jtwitter.Twitter;
 import android.app.Application;
 import android.content.SharedPreferences;
 import android.content.SharedPreferences.OnSharedPreferenceChangeListener;
 import android.preference.PreferenceManager;
 import android.widget.Button;
 
+import winterwell.jtwitter.Twitter;
+
 public class App extends Application {
-    public Button             lastSubmit;
+    public  Button            lastSubmit;
+    public  Button            lastRefresh;
     private Twitter           twitter;
     private SharedPreferences prefs;
 
     @Override
     public void onCreate() {
         initSharedPrefs();
-        prefs.registerOnSharedPreferenceChangeListener(new OnSharedPreferenceChangeListener() {
-            public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
-                initTwitter();
+        prefs.registerOnSharedPreferenceChangeListener( new OnSharedPreferenceChangeListener() {
+            public void onSharedPreferenceChanged( SharedPreferences sharedPreferences, String key ) {
+                twitter = null;
             }
-        });
+        } );
         super.onCreate();
     }
 
     public Twitter getTwitter() {
-        if (twitter == null) {
+        if ( twitter == null ) {
             initTwitter();
         }
         return twitter;
@@ -35,14 +37,14 @@ public class App extends Application {
     }
 
     private void initSharedPrefs() {
-        prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        prefs = PreferenceManager.getDefaultSharedPreferences( this );
     }
 
     private void initTwitter() {
-        String username = prefs.getString("PDM_USERNAME", "");
-        String password = prefs.getString("PDM_PASSWORD", "");
-        String url = prefs.getString("PDM_URL", "");
-        twitter = new Twitter(username, password);
-        twitter.setAPIRootUrl(url);
+        String username = prefs.getString( "user", "" );
+        String password = prefs.getString( "pass", "" );
+        String url = prefs.getString( "url", "" );
+        twitter = new Twitter( username, password );
+        twitter.setAPIRootUrl( url );
     }
 }
--- src/pt/isel/pdm/Yamba/TimelineActivity.java
+++ src/pt/isel/pdm/Yamba/TimelineActivity.java
@@ -1,31 +1,99 @@
 package pt.isel.pdm.Yamba;
 
+import java.util.ArrayList;
 import java.util.List;
 
-import winterwell.jtwitter.Twitter;
-import winterwell.jtwitter.Twitter.Status;
 import android.app.Activity;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+import winterwell.jtwitter.Twitter;
 
-public class TimelineActivity extends Activity {
+public class TimelineActivity extends Activity implements OnClickListener {
+
+    private List< TwitterStatus > timeline = initTimeline();
+
+    private ListView view;
+    private SimpleAdapter adapter;
+    private Button refreshButton;
     
-    private List<Status> timeline; 
-    
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        Twitter twitter = new Twitter();
-        if ( timeline == null ) {
-            timeline = twitter.getHomeTimeline();            
-        }
+    private static List< TwitterStatus > initTimeline() {
+        List< TwitterStatus > l = new ArrayList< TwitterStatus >();
+        return l;
     }
     
+    @Override
+    protected void onCreate( Bundle savedInstanceState ) {
+        setContentView( R.layout.timeline );
+        super.onCreate( savedInstanceState );
+        view = ( ListView ) findViewById( android.R.id.list );
+        
+        refreshButton = ( Button ) findViewById( R.id.button1 );
+        refreshButton.setOnClickListener( this );
+        
+        App app = (App) getApplication();
+        if (app.lastRefresh != null && !app.lastRefresh.isEnabled()) {
+            disableRefresh();
+        }
+        
+        app.lastRefresh = refreshButton;
+        
+        String[] from = { TwitterStatus.KEY_USER, TwitterStatus.KEY_DATE, TwitterStatus.KEY_TWEET };
+        int[] to = { R.id.user, R.id.date, R.id.tweet };        
+        adapter = new SimpleAdapter( this, timeline, R.layout.timeline_item, from, to );
+        view.setAdapter( adapter );
+        
+        updateTimeline();
+    }
+    
+    private class UpdateTimelineTask extends AsyncTask< Void, Void, Void > {
+
+        @Override
+        protected Void doInBackground( Void ... params ) {
+            List<Twitter.Status> twitterStatus = ((App) getApplication()).getTwitter().getPublicTimeline();
+            
+            for( Twitter.Status status : twitterStatus ) {
+                TwitterStatus temp = new TwitterStatus( status.getId(), status.getUser().getName(), status.getCreatedAt(), status.getText() );
+                timeline.add( temp );
+            }
+            return null;
+        }
+        
+        @Override
+        protected void onPostExecute( Void result ) {
+            adapter.notifyDataSetChanged();
+            enableRefresh();
+        }
+        
+    }
+    
+    private void updateTimeline() {
+        UpdateTimelineTask task = new UpdateTimelineTask();
+        timeline.clear();
+        task.execute();
+    }
     
     @Override
     protected void onDestroy() {
         super.onDestroy();
     }
+
+    public void onClick( View v ) {
+        disableRefresh();
+        updateTimeline();
+    }
     
+    private void disableRefresh() {
+        refreshButton.setEnabled( false );
+    }
     
-    
+    private void enableRefresh() {
+        Button refresh = ((App) getApplication()).lastRefresh;
+        refresh.setEnabled(true);        
+        
+    }    
 }
