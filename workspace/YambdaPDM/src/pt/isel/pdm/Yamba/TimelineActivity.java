d9d9ef3d46f2c862069b81bf1b7365ab9a15975b
Luís Miguel Dias Duarte <lduarte@luisduarte.net>
2012-04-14 16:50:14 +0000

    Synched with Github

--- workspace/YambdaPDM/src/pt/isel/pdm/Yamba/TimelineActivity.java
+++ workspace/YambdaPDM/src/pt/isel/pdm/Yamba/TimelineActivity.java
@@ -3,90 +3,143 @@ package pt.isel.pdm.Yamba;
 import java.util.ArrayList;
 import java.util.List;
 
-import winterwell.jtwitter.Twitter;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.text.format.DateUtils;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
+import android.widget.TextView;
 import android.widget.Toast;
+import winterwell.jtwitter.Twitter;
 
-public class TimelineActivity extends PreferencesEnabledActivity implements
-        OnClickListener {
+public class TimelineActivity extends PreferencesEnabledActivity implements OnClickListener, OnItemClickListener {
 
-    private List<TwitterStatus> timeline = initTimeline();
-    private boolean            isFirstTime = true;
-    
-    private ListView            view;
-    private SimpleAdapter       adapter;
-    private Button              refreshButton;
+    private static final String   TERMINATOR_SHORT_TEXT_TERMINATOR = "...";
 
-    private static List<TwitterStatus> initTimeline() {
-        List<TwitterStatus> l = new ArrayList<TwitterStatus>();
+    private static final int      MAX_CHARS_NO_LIMIT               = -1;
+
+    private static final int      DEFAULT_MAX_TWEETS               = -1;
+
+    private List< TwitterStatus > timeline                         = initTimeline();
+    private boolean               isFirstTime                      = true;
+
+    private ListView              view;
+    private SimpleAdapter         adapter;
+    private Button                refreshButton;
+
+    private static List< TwitterStatus > initTimeline() {
+        List< TwitterStatus > l = new ArrayList< TwitterStatus >();
         return l;
     }
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        setContentView(R.layout.timeline);
-        super.onCreate(savedInstanceState);
-        view = (ListView) findViewById(android.R.id.list);
+    protected void onCreate( Bundle savedInstanceState ) {
+        setContentView( R.layout.timeline );
+        super.onCreate( savedInstanceState );
+        view = ( ListView ) findViewById( android.R.id.list );
+        view.setOnItemClickListener( this );
 
-        refreshButton = (Button) findViewById(R.id.refreshButton);
-        refreshButton.setOnClickListener(this);
+        refreshButton = ( Button ) findViewById( R.id.refreshButton );
+        refreshButton.setOnClickListener( this );
 
-        App app = (App) getApplication();
-        if (app.lastRefresh != null && !app.lastRefresh.isEnabled() ) {
+        YambaPDMApplication app = ( YambaPDMApplication ) getApplication();
+        if ( app.lastRefresh != null && !app.lastRefresh.isEnabled() ) {
             disableRefresh();
         }
 
         app.lastRefresh = refreshButton;
 
-        String[] from = { TwitterStatus.KEY_USER, TwitterStatus.KEY_DATE,
-                TwitterStatus.KEY_TWEET };
+        String[] from = { TwitterStatus.KEY_USER, TwitterStatus.KEY_TIMESTAMP, TwitterStatus.KEY_TWEET };
         int[] to = { R.id.user, R.id.date, R.id.tweet };
-        adapter = new SimpleAdapter(this, timeline, R.layout.timeline_item,
-                from, to);
-        view.setAdapter(adapter);
-        
-        if( isFirstTime ) {
+        adapter = new TweetAdapter( this, timeline, R.layout.timeline_item, from, to );
+        view.setAdapter( adapter );
+
+        if ( isFirstTime ) {
             disableRefresh();
             updateTimeline();
-            isFirstTime = true;
+            isFirstTime = false;
         }
     }
 
-    private class UpdateTimelineTask extends AsyncTask<Void, Void, Void> {
+    private class TweetAdapter extends SimpleAdapter {
+
+        public TweetAdapter( Context context, List< TwitterStatus > data, int resource, String[] from, int[] to ) {
+            super( context, data, resource, from, to );
+        }
+
+        @Override
+        public void setViewText( TextView v, String text ) {
+            switch ( v.getId() ) {
+            case R.id.tweet: {
+                SharedPreferences prefs = ( ( YambaPDMApplication ) getApplication() ).getSharedPreferences();
+                int max_chars = Integer.valueOf( prefs.getString( PrefsActivity.KEY_MAX_PRESENTED_CHARS,
+                        String.valueOf( MAX_CHARS_NO_LIMIT ) ) );
+                if ( max_chars != MAX_CHARS_NO_LIMIT ) {
+                    String newText = text.substring( 0, max_chars - TERMINATOR_SHORT_TEXT_TERMINATOR.length() ).concat(
+                            TERMINATOR_SHORT_TEXT_TERMINATOR );
+                    v.setText( newText );
+                }
+                break;
+            }
+            case R.id.date: {
+                long tweetTimeStamp = Long.parseLong( text );
+                String relativeTime = DateUtils.getRelativeTimeSpanString( tweetTimeStamp ).toString();
+                v.setText( relativeTime );
+                break;
+            }
+            default: {
+                super.setViewText( v, text );
+                break;
+            }
+            }
+        }
+    }
+
+    private class UpdateTimelineTask extends AsyncTask< Void, Void, Void > {
 
         private volatile Exception exception;
 
         @Override
-        protected Void doInBackground(Void... params) {
+        protected Void doInBackground( Void ... params ) {
             try {
-                List<Twitter.Status> twitterStatus = ((App) getApplication())
-                        .getTwitter().getPublicTimeline();
-                for (Twitter.Status status : twitterStatus) {
-                    TwitterStatus temp = new TwitterStatus(status.getId(),
-                            status.getUser().getName(), status.getCreatedAt(),
-                            status.getText());
-                    timeline.add(temp);
+                List< Twitter.Status > twitterStatus = ( ( YambaPDMApplication ) getApplication() ).getTwitter()
+                        .getUserTimeline();
+
+                SharedPreferences prefs = ( ( YambaPDMApplication ) getApplication() ).getSharedPreferences();
+                int max_tweets = Integer.valueOf( prefs.getString( PrefsActivity.KEY_MAX_PRESENTED_TWEETS,
+                        String.valueOf( DEFAULT_MAX_TWEETS ) ) );
+
+                if ( max_tweets != DEFAULT_MAX_TWEETS ) {
+                    twitterStatus = twitterStatus.subList( 0, max_tweets );
                 }
-            } catch (Exception e) {
+
+                for ( Twitter.Status status : twitterStatus ) {
+                    TwitterStatus temp = new TwitterStatus( status.getId(), status.getUser().getName(),
+                            status.getCreatedAt(), status.getText() );
+                    timeline.add( temp );
+                }
+            } catch ( Exception e ) {
                 exception = e;
             }
             return null;
         }
 
         @Override
-        protected void onPostExecute(Void result) {
-            if (exception != null) {
-                Toast.makeText(TimelineActivity.this, exception.getMessage(),
-                        Toast.LENGTH_LONG).show();
-            } else {
-                adapter.notifyDataSetChanged();
+        protected void onPostExecute( Void result ) {
+            if ( exception != null ) {
+                Toast.makeText( TimelineActivity.this, exception.getMessage(), Toast.LENGTH_LONG ).show();
             }
+            adapter.notifyDataSetChanged();
+            Toast.makeText( TimelineActivity.this, "Fetched : " + timeline.size() + " elements", Toast.LENGTH_LONG )
+                    .show();
             enableRefresh();
         }
 
@@ -103,17 +156,29 @@ public class TimelineActivity extends PreferencesEnabledActivity implements
         super.onDestroy();
     }
 
-    public void onClick(View v) {
+    public void onClick( View v ) {
         disableRefresh();
         updateTimeline();
     }
 
     private void disableRefresh() {
-        refreshButton.setEnabled(false);
+        refreshButton.setEnabled( false );
     }
 
     private void enableRefresh() {
-        Button refresh = ((App) getApplication()).lastRefresh;
-        refresh.setEnabled(true);
+        Button refresh = ( ( YambaPDMApplication ) getApplication() ).lastRefresh;
+        refresh.setEnabled( true );
+    }
+
+    public void onItemClick( AdapterView< ? > parent, View view, int position, long id ) {
+        Intent intent = new Intent( this, DetailActivity.class );
+        intent.addFlags( Intent.FLAG_ACTIVITY_SINGLE_TOP );
+
+        intent.putExtra( TwitterStatus.KEY_TWEET, timeline.get( position ).getTweet() );
+        intent.putExtra( TwitterStatus.KEY_DATE, timeline.get( position ).getDate() );
+        intent.putExtra( TwitterStatus.KEY_USER, timeline.get( position ).getUser() );
+        intent.putExtra( TwitterStatus.KEY_ID, timeline.get( position ).getId() );
+
+        startActivity( intent );
     }
 }
