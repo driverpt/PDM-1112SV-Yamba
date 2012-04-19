c23dede59df6d471de7672b8559abd28df98ce7d
André Domingues <avdomingos@gmail.com>
2012-04-19 21:45:18 +0000

    Criado packages para services Criadas classes para publicação de status via serviço e actualização do Timeline

---
+++ workspace/YambdaPDM/src/pt/isel/pdm/Yamba/services/TimelineService.java
@@ -0,0 +1,18 @@
+package pt.isel.pdm.Yamba.services;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+public class TimelineService extends Service implements Runnable{
+
+  public void run() {
+    // TODO Auto-generated method stub
+  }
+
+  @Override
+  public IBinder onBind(Intent intent) {
+    // TODO Auto-generated method stub
+    return null;
+  }
+}
