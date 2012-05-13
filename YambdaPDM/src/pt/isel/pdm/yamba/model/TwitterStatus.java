55b5544260bc4c5a427d6e25e880f820e4f3059a
Hugo Ferreira <hugoferreira4@gmail.com>
2012-05-13 13:55:57 +0000

    New timeline theme

--- YambdaPDM/src/pt/isel/pdm/yamba/model/TwitterStatus.java
+++ YambdaPDM/src/pt/isel/pdm/yamba/model/TwitterStatus.java
@@ -1,5 +1,6 @@
 package pt.isel.pdm.yamba.model;
 
+import java.net.URI;
 import java.util.Date;
 import java.util.TreeMap;
 
@@ -7,49 +8,45 @@ import android.net.Uri;
 
 @SuppressWarnings( "serial" )
 public class TwitterStatus extends TreeMap< String, Object > {
-	public static final String KEY_ID        = "pdm_id";
-	public static final String KEY_DATE      = "pdm_date";
-	public static final String KEY_TIMESTAMP = "pdm_timestamp";
-	public static final String KEY_USER      = "pdm_user";
-	public static final String KEY_TWEET     = "pdm_tweet";
-	public static final String KEY_PHOTO_URI = "pdm_photoUri";
+    public static final String KEY_ID        = "pdm_id";
+    public static final String KEY_DATE      = "pdm_date";
+    public static final String KEY_TIMESTAMP = "pdm_timestamp";
+    public static final String KEY_USER      = "pdm_user";
+    public static final String KEY_TWEET     = "pdm_tweet";
+    public static final String KEY_PHOTO_URI = "pdm_photoUri";
 
-	public TwitterStatus( long id, String user, Date date, String tweet) {
-		putData(id, user, date, tweet, null);
-	}
+    public TwitterStatus( long id, String user, Date date, String tweet, URI photoUri) {
+        putData(id, user, date, tweet, photoUri);
+    }
 
-	public TwitterStatus( long id, String user, Date date, String tweet, Uri photoUri) {
-		putData(id, user, date, tweet, photoUri);
-	}
+    private void putData(long id, String user, Date date, String tweet, URI pictureUri) {
+        put( KEY_ID, id );
+        put( KEY_DATE, date );
+        put( KEY_USER, user );
+        put( KEY_TWEET, tweet );
+        put( KEY_TIMESTAMP, date.getTime());
+        if (pictureUri != null)
+            put(KEY_PHOTO_URI, pictureUri);
+    }
 
-	private void putData(long id, String user, Date date, String tweet, Uri pictureUri) {
-		put( KEY_ID, id );
-		put( KEY_DATE, date );
-		put( KEY_USER, user );
-		put( KEY_TWEET, tweet );
-		put( KEY_TIMESTAMP, date.getTime());
-		if (pictureUri != null)
-			put(KEY_PHOTO_URI, pictureUri);
-	}
+    public long getId() {
+        return ( ( Long ) get( KEY_ID ) ).longValue();
+    }
 
-	public long getId() {
-		return ( ( Long ) get( KEY_ID ) ).longValue();
-	}
+    public Date getDate() {
+        return ( Date ) get( KEY_DATE );
+    }
 
-	public Date getDate() {
-		return ( Date ) get( KEY_DATE );
-	}
+    public String getUser() {
+        return ( String ) get( KEY_USER );
+    }
 
-	public String getUser() {
-		return ( String ) get( KEY_USER );
-	}
+    public String getTweet() {
+        return ( String ) get( KEY_TWEET );
+    }
 
-	public String getTweet() {
-		return ( String ) get( KEY_TWEET );
-	}
-
-	public Uri getPhotoUri()
-	{
-		return (Uri)get(KEY_PHOTO_URI);
-	}
+    public URI getPhotoUri()
+    {
+        return (URI)get(KEY_PHOTO_URI);
+    }
 }
