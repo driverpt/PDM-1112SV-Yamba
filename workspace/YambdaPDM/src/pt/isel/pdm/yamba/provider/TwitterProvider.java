75ff794add2fe90737186f1bb343be8d7b2be84d
Hugo Ferreira <hugoferreira4@gmail.com>
2012-05-26 18:21:56 +0000

    TwitterProvider.java

--- workspace/YambdaPDM/src/pt/isel/pdm/yamba/provider/TwitterProvider.java
+++ workspace/YambdaPDM/src/pt/isel/pdm/yamba/provider/TwitterProvider.java
@@ -18,15 +18,15 @@ public class TwitterProvider  extends ContentProvider{
     public static final Uri CONTENT_URI = 
         Uri.parse(ContentResolver.SCHEME_CONTENT+"://"+AUTHORITY);
 
-    private static final int TWITTER_ALL = 1;
-    private static final int TWITTER_ID = 2;
+    private static final int TWEET_ALL = 1;
+    private static final int TWEET_ID = 2;
     private static final int USER_ALL = 3;
     private static final int USER_ID = 4;
 
     private static final UriMatcher uriMatcher = new UriMatcher(UriMatcher.NO_MATCH);
     static {
-        uriMatcher.addURI(AUTHORITY, "TWEET", TWITTER_ALL);
-        uriMatcher.addURI(AUTHORITY, "TWEET/#", TWITTER_ID);
+        uriMatcher.addURI(AUTHORITY, "TWEET", TWEET_ALL);
+        uriMatcher.addURI(AUTHORITY, "TWEET/#", TWEET_ID);
         uriMatcher.addURI(AUTHORITY, "USER", USER_ID);
         uriMatcher.addURI(AUTHORITY, "USER/#", USER_ID);
     }
@@ -40,63 +40,90 @@ public class TwitterProvider  extends ContentProvider{
     @Override
     public Cursor query(Uri uri, String[] projection, String selection,
             String[] selectionArgs, String sortOrder) {
-        selection = makeSelection(uri, selection);
+        SelectionParameters args = makeSelection(uri, selection);
         SQLiteDatabase db = thp.getReadableDatabase();
-        return db.query(TweetContract.TABLE, projection, selection, selectionArgs, null, null, sortOrder);
+        return db.query(args.getTable(), projection, args.getSelection(), selectionArgs, null, null, sortOrder);
     }
-
-    private String makeSelection(Uri uri, String sel) {
-        switch (uriMatcher.match(uri)) {
-        case TWITTER_ID: return TweetContract._ID + "=" + uri.getLastPathSegment();
-        case USER_ID:    return UserContract._ID + "=" + uri.getLastPathSegment();
-        default:         return sel;
-        }
-    }
-
     @Override
     public Uri insert(Uri uri, ContentValues values) {
         SQLiteDatabase db = thp.getWritableDatabase();
         try {
-            long row = db.insert(TweetContract.TABLE, null, values);
+            String table = getTable(uri);
+            long row = db.insert(table, null, values);
             return (row==-1) ? null : ContentUris.withAppendedId(uri, row);
         } finally { db.close(); }
     }
-
     @Override
     public int update(Uri uri, ContentValues values, String selection,
             String[] selectionArgs) {
         SQLiteDatabase db = thp.getWritableDatabase();
         try {
-            selection = makeSelection(uri, selection);
-            return db.update(TweetContract.TABLE, values, selection, selectionArgs);
+            SelectionParameters args = makeSelection(uri, selection);
+            return db.update(args.getTable(), values, args.getSelection(), selectionArgs);
         } finally { db.close(); }
     }
-
     @Override
     public int delete(Uri uri, String selection, String[] selectionArgs) {
         SQLiteDatabase db = thp.getWritableDatabase();
         try {
-            selection = makeSelection(uri, selection);
-            return db.delete(TweetContract.TABLE, selection, selectionArgs);
+            SelectionParameters args = makeSelection(uri, selection);
+            return db.delete(args.getTable(), args.getSelection(), selectionArgs);
         } finally { db.close(); }
     }
 
+    private class SelectionParameters{
+        public String _table;
+        public String _selection;
+
+        public SelectionParameters(String table, String selection){
+            _table = table;
+            _selection = selection;
+        }
+
+        public String getSelection(){ return _selection; }
+        public String getTable(){ return _table; }
+    }
+    private SelectionParameters makeSelection(Uri uri, String sel) {
+        switch (uriMatcher.match(uri)) {
+        case TWEET_ALL: 
+            return new SelectionParameters(TweetContract.TABLE,sel);
+        case TWEET_ID: 
+            return new SelectionParameters(TweetContract.TABLE,TweetContract._ID + "=" + uri.getLastPathSegment());
+        case USER_ALL: 
+            return new SelectionParameters(UserContract.TABLE,sel);
+        case USER_ID:    
+            return new SelectionParameters(UserContract.TABLE,UserContract._ID + "=" + uri.getLastPathSegment());
+        default:  
+            return null;
+        }
+    }
+    private String getTable(Uri uri){
+        switch (uriMatcher.match(uri)) {
+        case TWEET_ALL: 
+        case TWEET_ID: 
+            return TweetContract.TABLE;
+        case USER_ALL: 
+        case USER_ID:    
+            return UserContract.TABLE;
+        default:  
+            return null;
+        }
+    }
+    
     private static final String MIME_BASE_DIR = "vnd.android.cursor.dir/vnd.pt.isel.pdm.yamba";
     private static final String MIME_BASE_ITEM = "vnd.android.cursor.item/vnd.pt.isel.pdm.yamba";
-
     private static final String MIME_TWEET_ALL = MIME_BASE_DIR +  ".tweet";
     private static final String MIME_TWEET_ONE = MIME_BASE_ITEM + ".tweet";
     private static final String MIME_USER_ALL = MIME_BASE_DIR +  ".user";
     private static final String MIME_USER_ONE = MIME_BASE_ITEM + ".user";
-
     @Override
     public String getType(Uri uri) {
         int match = uriMatcher.match(uri);
         switch (match)
         {
-        case TWITTER_ALL:
+        case TWEET_ALL:
             return MIME_TWEET_ALL;
-        case TWITTER_ID:
+        case TWEET_ID:
             return MIME_TWEET_ONE;
         case USER_ALL:
             return MIME_USER_ALL;
