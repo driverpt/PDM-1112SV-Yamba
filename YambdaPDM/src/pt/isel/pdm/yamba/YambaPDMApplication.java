bd676cf6a97de978f86d7ae6464d3b75df2b01b7
André Domingues <avdomingos@gmail.com>
2012-05-27 17:38:18 +0000

    Alteração para iniciar um startedService quando é alterada a pref de automatic update (ainda não está completo)

--- YambdaPDM/src/pt/isel/pdm/yamba/YambaPDMApplication.java
+++ YambdaPDM/src/pt/isel/pdm/yamba/YambaPDMApplication.java
@@ -15,119 +15,138 @@ import android.widget.Button;
 
 import pt.isel.pdm.yamba.model.TwitterStatus;
 import pt.isel.pdm.yamba.services.TimelineService;
+import pt.isel.pdm.yamba.services.WifiTimelineAutoUpdaterService;
 
 import winterwell.jtwitter.Twitter;
 
 public class YambaPDMApplication extends Application implements OnSharedPreferenceChangeListener, Runnable {
-    public Button                         lastSubmit;
-    public Button                         lastRefresh;
-    private Twitter                       twitter;
-    private SharedPreferences             prefs;
+	public Button                         lastSubmit;
+	public Button                         lastRefresh;
+	private Twitter                       twitter;
+	private SharedPreferences             prefs;
 
-    private List< Twitter.Status >        timeline;
-    private OnYambaTimelineChangeListener timelineChangedListener;
+	private List< Twitter.Status >        timeline;
+	private OnYambaTimelineChangeListener timelineChangedListener;
 
-    private Runnable                      lastTimelineServiceRunnableToken;
-    private Handler                       handler;
+	private WifiTimelineAutoUpdaterService timelineService;
 
-    @Override
-    public void onCreate() {
-        initSharedPrefs();
-        prefs.registerOnSharedPreferenceChangeListener( this );
-        handler = new Handler();
-        super.onCreate();
-    }
+	private Runnable                      lastTimelineServiceRunnableToken;
+	private Handler                       handler;
 
-    public Twitter getTwitter() {
-        if ( twitter == null ) {
-            initTwitter();
-        }
-        return twitter;
-    }
 
-    public void setOnYambaTimelineChangeListener( OnYambaTimelineChangeListener listener ) {
-        timelineChangedListener = listener;
-    }
+	@Override
+	public void onCreate() {
+		initSharedPrefs();
+		prefs.registerOnSharedPreferenceChangeListener( this );
+		handler = new Handler();
+		super.onCreate();
+	}
 
-    public List< Twitter.Status > getCurrentTimeline() {
-        return timeline;
-    }
+	public Twitter getTwitter() {
+		if ( twitter == null ) {
+			initTwitter();
+		}
+		return twitter;
+	}
 
-    // Synchronized is needed to avoid concurrency issues
-    public synchronized void setTimeline( List< Twitter.Status > newTimeline ) {
-        timeline = newTimeline;
-        if ( timelineChangedListener != null ) {
-            notifyOnYambaTimelineChangeListener();
-        }
-    }
+	public void setOnYambaTimelineChangeListener( OnYambaTimelineChangeListener listener ) {
+		timelineChangedListener = listener;
+	}
 
-    private void notifyOnYambaTimelineChangeListener() {
-        Handler handler = new Handler( Looper.getMainLooper() );
-        handler.post( new Runnable() {
-            public void run() {
-                timelineChangedListener.onYambaTimelineChange();
-            }
-        } );
-    }
+	public List< Twitter.Status > getCurrentTimeline() {
+		return timeline;
+	}
 
-    public void onSharedPreferenceChanged( SharedPreferences sharedPreferences, String key ) {
-        if ( key.equals( PrefsActivity.KEY_USERNAME ) || key.equals( PrefsActivity.KEY_URL )
-                || key.equals( PrefsActivity.KEY_PASSWORD ) ) {
-            twitter = null;
-        }
-        if ( key.equals( PrefsActivity.KEY_TIMELINE_REFRESH ) ) {
-            scheduleTimelineService();
-        }
-    }
+	// Synchronized is needed to avoid concurrency issues
+	public synchronized void setTimeline( List< Twitter.Status > newTimeline ) {
+		timeline = newTimeline;
+		if ( timelineChangedListener != null ) {
+			notifyOnYambaTimelineChangeListener();
+		}
+	}
 
-    public void scheduleTimelineService() {
-        if ( lastTimelineServiceRunnableToken != null ) {
-            handler.removeCallbacks( lastTimelineServiceRunnableToken, lastTimelineServiceRunnableToken );
-        }
-        publishRunnableOnTimelineService( this );
-    }
+	private void notifyOnYambaTimelineChangeListener() {
+		Handler handler = new Handler( Looper.getMainLooper() );
+		handler.post( new Runnable() {
+			public void run() {
+				timelineChangedListener.onYambaTimelineChange();
+			}
+		} );
+	}
 
-    public void publishRunnableOnTimelineService( Runnable runnable ) {
-        int updateInterval = Integer.parseInt( getSharedPreferences().getString( PrefsActivity.KEY_TIMELINE_REFRESH, "0" ) );
+	public void onSharedPreferenceChanged( SharedPreferences sharedPreferences, String key ) {
+		if ( key.equals( PrefsActivity.KEY_USERNAME ) || key.equals( PrefsActivity.KEY_URL )
+				|| key.equals( PrefsActivity.KEY_PASSWORD ) ) {
+			twitter = null;
+		}
+		if ( key.equals( PrefsActivity.KEY_TIMELINE_REFRESH ) ) {
+			scheduleTimelineService();
+		}
+	}
 
-        if ( updateInterval == 0 ) {
-            return;
-        }
+	public void scheduleTimelineService() {
+		if ( lastTimelineServiceRunnableToken != null ) {
+			handler.removeCallbacks( lastTimelineServiceRunnableToken, lastTimelineServiceRunnableToken );
+		}
+		publishRunnableOnTimelineService( this );
+	}
 
-        long uptimeMillis = SystemClock.uptimeMillis();
+	public void publishRunnableOnTimelineService( Runnable runnable ) {
+		int updateInterval = Integer.parseInt( getSharedPreferences().getString( PrefsActivity.KEY_TIMELINE_REFRESH, "0" ) );
 
-        TimeUnit timeunit = TimeUnit.MILLISECONDS;
-        long delay = timeunit.convert( updateInterval, TimeUnit.MINUTES );
+		if ( updateInterval == 0 ) {
+			return;
+		}
 
-        uptimeMillis += delay;
+		long uptimeMillis = SystemClock.uptimeMillis();
 
-        handler.postAtTime( runnable, runnable, uptimeMillis );
-        lastTimelineServiceRunnableToken = runnable;
-    }
+		TimeUnit timeunit = TimeUnit.MILLISECONDS;
+		long delay = timeunit.convert( updateInterval, TimeUnit.MINUTES );
 
-    public SharedPreferences getSharedPreferences() {
-        return prefs;
-    }
+		uptimeMillis += delay;
 
-    private void initSharedPrefs() {
-        prefs = PreferenceManager.getDefaultSharedPreferences( this );
-    }
+		handler.postAtTime( runnable, runnable, uptimeMillis );
+		lastTimelineServiceRunnableToken = runnable;
+	}
 
-    private void initTwitter() {
-        String username = prefs.getString( PrefsActivity.KEY_USERNAME, "" );
-        String password = prefs.getString( PrefsActivity.KEY_PASSWORD, "" );
-        String url = prefs.getString( PrefsActivity.KEY_URL, "" );
-        twitter = new Twitter( username, password );
-        twitter.setAPIRootUrl( url );
-    }
 
-    public void run() {
-        Intent intent = new Intent( this, TimelineService.class );
-        intent.putExtra( TimelineService.OPERATION, TimelineService.MSG_UPDATE_TIMELINE );
-        startService( intent );
-    }
+	public SharedPreferences getSharedPreferences() {
+		return prefs;
+	}
+
+	private void initSharedPrefs() {
+		prefs = PreferenceManager.getDefaultSharedPreferences( this );
+	}
+
+	private void initTwitter() {
+		String username = prefs.getString( PrefsActivity.KEY_USERNAME, "" );
+		String password = prefs.getString( PrefsActivity.KEY_PASSWORD, "" );
+		String url = prefs.getString( PrefsActivity.KEY_URL, "" );
+		twitter = new Twitter( username, password );
+		twitter.setAPIRootUrl( url );
+	}
+
+	public void run() {
+		Intent intent = new Intent( this, TimelineService.class );
+		intent.putExtra( TimelineService.OPERATION, TimelineService.MSG_UPDATE_TIMELINE );
+		startService( intent );
+	}
 
 	public List<Twitter.Status> getTimeline() {
 		return timeline;
+	}
+
+
+	public void startTimelineAutomaticUpdates()
+	{
+		if (timelineService == null)
+			timelineService = new WifiTimelineAutoUpdaterService();
+		Intent intent = new Intent(this, WifiTimelineAutoUpdaterService.class);
+		startService(intent);
+	}
+
+	public void stopTimelineAutomaticUpdates() {
+		WifiTimelineAutoUpdaterService.serviceInstance.stopSelf();
+
 	}
 }
