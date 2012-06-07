2b69822ff434e420ff6673d14dbc7daec621da7c
Luís Miguel Dias Duarte <lduarte@luisduarte.net>
2012-06-07 00:04:35 +0000

    Started Adaptation of the StatusPublishingService to the new Architecture.
    TODO: Create SQLiteOpenHelper for each table or we'll have some trouble :(

--- workspace/YambdaPDM/src/pt/isel/pdm/yamba/services/StatusPublishingService.java
+++ workspace/YambdaPDM/src/pt/isel/pdm/yamba/services/StatusPublishingService.java
@@ -1,11 +1,19 @@
 package pt.isel.pdm.yamba.services;
 
+import java.util.Date;
+
 import android.content.Intent;
 import android.os.Handler;
 import android.util.Log;
 import android.widget.Toast;
 
+import pt.isel.pdm.yamba.dataAccessLayer.TweetDataAccessLayer;
+
+import winterwell.jtwitter.Twitter.Status;
+
 import pt.isel.pdm.yamba.YambaPDMApplication;
+import pt.isel.pdm.yamba.dataAccessLayer.TweetToPostDataAccessLayer;
+import pt.isel.pdm.yamba.model.TweetToPost;
 import winterwell.jtwitter.Twitter;
 
 public class StatusPublishingService extends ConnectivityAwareIntentService {
@@ -18,7 +26,7 @@ public class StatusPublishingService extends ConnectivityAwareIntentService {
 
     private Twitter             twitter;
     private Handler             mainHandler;
-
+  
     public StatusPublishingService() {
         super( SERVICE_NAME );
     }
@@ -37,14 +45,18 @@ public class StatusPublishingService extends ConnectivityAwareIntentService {
         Log.d( LOGGER_TAG, "StatusPublishingService.onHandleIntent()" );
         String message = intent.getStringExtra( TWEET_MSG );
         Log.d( LOGGER_TAG, String.format( "Tweet Message: %s", intent.getStringExtra( TWEET_MSG ) ) );
+        if ( !hasConnectivity() ) {
+            Log.d( LOGGER_TAG, "No Wifi Connectivity Available, storing new message in local database" );
+            TweetToPost tweet = new TweetToPost( new Date(), message );
+            TweetToPostDataAccessLayer.insertTweetToPost( getContentResolver(), tweet );
+            return;
+        }
+        
         try {
-            if( hasConnectivity() ) {
-                twitter.updateStatus( message );
-                Log.d( LOGGER_TAG, String.format( "Message Sucessfully Posted" ) );
-            }
-            else {
-                // TODO: Store in ContentProvider for future publishing
-            }
+            Status status = twitter.updateStatus( message );
+            TweetDataAccessLayer.insertTweet( getContentResolver(), status );
+            Log.d( LOGGER_TAG, String.format( "Message Sucessfully Posted" ) );
+            
         } catch ( final Exception e ) {
             mainHandler.post( new Runnable() {
                 public void run() {
@@ -63,6 +75,13 @@ public class StatusPublishingService extends ConnectivityAwareIntentService {
 
     @Override
     protected void onConnectivityAvailable() {
-        // TODO: Retrieve data from ContentProvider and start publishing it into Twitter
+        Log.d( LOGGER_TAG, "StatusPublishingService.onConnectivityAvailable()" );
+        super.onConnectivityAvailable();
+    }
+    
+    @Override
+    protected void onConnectivityUnavailable() {
+        Log.d( LOGGER_TAG, "StatusPublishingService.onConnectivityUnavailable()" );
+        super.onConnectivityUnavailable();
     }
 }
