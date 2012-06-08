8ceb0d75eedbe386f11b91e888fd0f992b78d1b8
Luís Miguel Dias Duarte <lduarte@luisduarte.net>
2012-06-08 16:38:51 +0000

    Added new Custom BroadcastReceiver.
    This BroadcastReceiver detects if there is network changes in connectivity and calls a method defined by the programmer on instantiation of the action to execute.

---
+++ workspace/YambdaPDM/src/pt/isel/pdm/yamba/ConnectivityAwareBroadcastReceiver.java
@@ -0,0 +1,40 @@
+package pt.isel.pdm.yamba;
+
+import android.app.Activity;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.util.Log;
+
+public abstract class ConnectivityAwareBroadcastReceiver extends BroadcastReceiver {
+    
+    private volatile boolean mHasConnectivity = false;
+    
+    @Override
+    public void onReceive( Context context, Intent intent ) {
+        ConnectivityManager mConnectivityManager = ( ConnectivityManager ) context.getSystemService( Activity.CONNECTIVITY_SERVICE );
+        boolean hasNetwork = !intent.getBooleanExtra( ConnectivityManager.EXTRA_NO_CONNECTIVITY, false );
+        boolean newNetworkState = false;
+        if ( hasNetwork ) {
+            NetworkInfo networkInfo = mConnectivityManager.getActiveNetworkInfo();
+            newNetworkState = onCheckConnectivityChange( networkInfo ); 
+        }
+        
+        if ( newNetworkState != mHasConnectivity ) {
+            mHasConnectivity = newNetworkState;
+            onPostConnectivityChange();
+        }
+    }
+    
+    protected boolean onCheckConnectivityChange( NetworkInfo networkInfo ) {
+        return ( networkInfo != null && networkInfo.isConnected() );
+    }
+    
+    public abstract void onPostConnectivityChange();
+    
+    public final boolean hasConnectivity() {
+        return mHasConnectivity;
+    }
+}
