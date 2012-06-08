321aa1b318406f92185efdbcc4447d6e1e85609c
Luís Miguel Dias Duarte <lduarte@luisduarte.net>
2012-06-08 16:46:24 +0000

    Timeline Activity now properly sets the ViewHolder as View Tag.
    Adapter TimelineActivity to YambaPost changes.

--- workspace/YambdaPDM/src/pt/isel/pdm/yamba/TimelineActivity.java
+++ workspace/YambdaPDM/src/pt/isel/pdm/yamba/TimelineActivity.java
@@ -1,6 +1,6 @@
 package pt.isel.pdm.yamba;
 
-import java.util.ArrayList;
+import java.util.Date;
 
 import android.content.BroadcastReceiver;
 import android.content.Context;
@@ -21,13 +21,15 @@ import android.view.View.OnClickListener;
 import android.widget.AdapterView;
 import android.widget.AdapterView.AdapterContextMenuInfo;
 import android.widget.AdapterView.OnItemClickListener;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.CursorAdapter;
+import android.widget.ImageView;
 import android.widget.ListView;
-import android.widget.SimpleCursorAdapter;
+import android.widget.ResourceCursorAdapter;
 import android.widget.TextView;
 
 import pt.isel.pdm.yamba.model.YambaPost;
+import pt.isel.pdm.yamba.model.YambaUser;
 import pt.isel.pdm.yamba.provider.contract.TweetContract;
 import pt.isel.pdm.yamba.services.TimelineService;
 
@@ -39,23 +41,26 @@ public class TimelineActivity extends PreferencesEnabledActivity implements OnCl
     private static final int          MAX_CHARS_NO_LIMIT               = 140;
     private static final int          DEFAULT_MAX_TWEETS               = 50;
 
-    private ArrayList< YambaPost >    timeline                         = initTimeline();
     private boolean                   isFirstTime                      = true;
 
     private ListView                  view;
-    private ArrayAdapter< YambaPost > adapter;
-    private SimpleCursorAdapter       mCursorAdapter;
+    private CursorAdapter             mCursorAdapter;
     private Button                    refreshButton;
 
     private BroadcastReceiver         mReceiver;
 
-    private  YambaPDMApplication      app;
-    
+    private YambaPDMApplication       app;
+
     private class TimelineUpdatedBroadcastReceiver extends BroadcastReceiver {
         @Override
         public void onReceive( Context context, Intent intent ) {
-            Log.d(LOGGER_TAG, "TimelineUpdatedBroadcastReceiver.onReceive() called");
-            updateCursor();
+            Log.d( LOGGER_TAG, "TimelineUpdatedBroadcastReceiver.onReceive() called" );
+            int operationResult = intent.getIntExtra( TimelineService.OPERATION, TimelineService.INVALID_OPERATION );
+            if ( operationResult == TimelineService.TIMELINE_UPDATE_OK ) {
+                updateCursor();
+            } else {
+                enableRefresh();
+            }
         }
     }
 
@@ -65,15 +70,10 @@ public class TimelineActivity extends PreferencesEnabledActivity implements OnCl
         mReceiver = new TimelineUpdatedBroadcastReceiver();
     }
 
-    private static ArrayList< YambaPost > initTimeline() {
-        ArrayList< YambaPost > l = new ArrayList< YambaPost >();
-        return l;
-    }
-
     @Override
     protected void onCreate( Bundle savedInstanceState ) {
         setContentView( R.layout.timeline );
-        
+
         super.onCreate( savedInstanceState );
         view = ( ListView ) findViewById( android.R.id.list );
 
@@ -81,52 +81,88 @@ public class TimelineActivity extends PreferencesEnabledActivity implements OnCl
         registerForContextMenu( view );
 
         app = ( YambaPDMApplication ) getApplication();
-        
+
         view.setOnItemClickListener( this );
 
         refreshButton = ( Button ) findViewById( R.id.refreshButton );
         refreshButton.setOnClickListener( this );
-               
+
         YambaPDMApplication app = ( YambaPDMApplication ) getApplication();
         // if ( app.lastRefresh != null && !app.lastRefresh.isEnabled() ) {
         disableRefresh();
         // }
 
         app.lastRefresh = refreshButton;
-        
-        mCursorAdapter = new TweetCursorAdapter( this, R.layout.timeline_item, null );
-        
+
+        mCursorAdapter = new TweetCursorAdapter( this, null );
+
         view.setAdapter( mCursorAdapter );
-       
+
         LocalBroadcastManager.getInstance( this ).registerReceiver( mReceiver, mReceiverFilter );
-        
+
         if ( isFirstTime ) {
             updateCursor();
             isFirstTime = false;
         }
     }
 
-    private void updateCursor() {       
+    private void updateCursor() {
         TimelineCursorUpdateTask task = new TimelineCursorUpdateTask();
         task.execute();
     }
-    
+
+    private class TweetCursorAdapter extends ResourceCursorAdapter {
+
+        public TweetCursorAdapter( Context context, Cursor c ) {
+            super( context, R.layout.timeline_item, c );
+        }
+
+        class ViewHolder {
+            public ImageView photo;
+            public TextView  user;
+            public TextView  date;
+            public TextView  tweet;
+        }
+
+        @Override
+        public void bindView( View view, Context context, Cursor cursor ) {
+            ImageView photoView = ( ImageView ) view.findViewById( R.id.photoUri );
+            TextView userView = ( TextView ) view.findViewById( R.id.user );
+            TextView tweetView = ( TextView ) view.findViewById( R.id.tweet );
+            TextView dateView = ( TextView ) view.findViewById( R.id.date );
+            
+            ViewHolder holder = new ViewHolder();
+            holder.photo = photoView;
+            holder.user = userView;
+            holder.date = dateView;
+            holder.tweet = tweetView;
+            
+            view.setTag( holder );
+            
+            String screenName = cursor.getString( cursor.getColumnIndex( TweetContract.USER ) );
+            long timestamp = cursor.getLong( cursor.getColumnIndex( TweetContract.TIMESTAMP ) );           
+            String tweet = cursor.getString( cursor.getColumnIndex( TweetContract.TWEET ) );
+            
+            dateView.setText( DateUtils.getRelativeTimeSpanString( timestamp ).toString() );
+            tweetView.setText( tweet );
+            userView.setText( screenName );
+        }
+    }
+
     private class TimelineCursorUpdateTask extends AsyncTask< Void, Void, Cursor > {
 
         @Override
         protected Cursor doInBackground( Void ... params ) {
-            Cursor mNewCursor = getContentResolver().query( TweetContract.CONTENT_URI
-                                                          , new String[] { TweetContract._ID, TweetContract.TIMESTAMP, TweetContract.TWEET, TweetContract.USER }
-                                                          , null
-                                                          , null
-                                                          , "timestamp DESC"
-                                                          );
+            Cursor mNewCursor = getContentResolver()
+                    .query( TweetContract.CONTENT_URI,
+                            new String[] { TweetContract._ID, TweetContract.TIMESTAMP, TweetContract.TWEET,
+                                    TweetContract.USER }, null, null, "timestamp DESC" );
             return mNewCursor;
         }
-        
+
         @Override
         protected void onPostExecute( Cursor result ) {
-            if( result != null ) {
+            if ( result != null ) {
                 mCursorAdapter.changeCursor( result );
                 mCursorAdapter.notifyDataSetChanged();
             }
@@ -134,88 +170,6 @@ public class TimelineActivity extends PreferencesEnabledActivity implements OnCl
             super.onPostExecute( result );
         }
     }
-    
-    static final String[] FROM = { TweetContract.TIMESTAMP, TweetContract.TWEET, TweetContract.USER };
-    static final int[] TO = new int[] { R.id.date, R.id.tweet, R.id.user };
-    
-    private class TweetCursorAdapter extends SimpleCursorAdapter {
-
-        public TweetCursorAdapter( Context context, int layout, Cursor c ) {
-            super( context, R.layout.timeline_item, c, FROM, TO );
-            setViewBinder( new TweetCursorAdapterViewBinder() );
-        }
-        
-        @Override
-        public void bindView( View view, Context context, Cursor cursor ) {
-            super.bindView( view, context, cursor );            
-            Log.d(LOGGER_TAG, "bindView() from TweetCursorAdapter called");
-        }
-        
-        private class TweetCursorAdapterViewBinder implements ViewBinder {
-            public boolean setViewValue( View arg0, Cursor arg1, int arg2 ) {
-                switch( arg0.getId() ) {
-                    case R.id.date:
-                        long timestamp = arg1.getLong( arg1.getColumnIndex( TweetContract.TIMESTAMP ) );
-                        (( TextView ) arg0).setText( DateUtils.getRelativeTimeSpanString( timestamp ).toString() );                        
-                        return true;
-                    case R.id.user:
-                        String user = arg1.getString( arg1.getColumnIndex( TweetContract.USER ) );
-                        (( TextView ) arg0).setText( user );
-                        return true;
-                    case R.id.tweet:
-                        String tweet = arg1.getString( arg1.getColumnIndex( TweetContract.TWEET ) );
-                        (( TextView ) arg0).setText( tweet );
-                        return true;
-                }
-                return false;
-            }
-        }
-    }
-    
-//    private class TweetAdapter extends ArrayAdapter< YambaPost > {
-//        private ArrayList< YambaPost > entries;
-//        private Activity               activity;
-//
-//        public TweetAdapter( Activity a, int textViewResourceId, ArrayList< YambaPost > entries ) {
-//            super( a, textViewResourceId, entries );
-//            this.entries = entries;
-//            this.activity = a;
-//        }
-//
-//        class ViewHolder {
-//            public ImageView photo;
-//            public TextView  user;
-//            public TextView  date;
-//            public TextView  tweet;
-//        }
-//
-//        @Override
-//        public View getView( int position, View convertView, ViewGroup parent ) {
-//            View v = convertView;
-//            ViewHolder holder;
-//            if ( v == null ) {
-//                LayoutInflater vi = ( LayoutInflater ) activity.getSystemService( Context.LAYOUT_INFLATER_SERVICE );
-//                v = vi.inflate( R.layout.timeline_item, null );
-//                holder = new ViewHolder();
-//                holder.photo = ( ImageView ) v.findViewById( R.id.photoUri );
-//                holder.user = ( TextView ) v.findViewById( R.id.user );
-//                holder.date = ( TextView ) v.findViewById( R.id.date );
-//                holder.tweet = ( TextView ) v.findViewById( R.id.tweet );
-//                v.setTag( holder );
-//            } else
-//                holder = ( ViewHolder ) v.getTag();
-//
-//            final YambaPost twitterStatus = entries.get( position );
-//            if ( twitterStatus != null ) {
-//                // holder.photo.setImageBitmap(bm);
-//                holder.user.setText( twitterStatus.getUser().getUsername() );
-//                holder.date.setText( DateUtils.getRelativeTimeSpanString( twitterStatus.getDate().getTime() )
-//                        .toString() );
-//                holder.tweet.setText( twitterStatus.getTweet() );
-//            }
-//            return v;
-//        }
-//    }
 
     private void updateTimeline() {
         disableRefresh();
@@ -228,7 +182,7 @@ public class TimelineActivity extends PreferencesEnabledActivity implements OnCl
     @Override
     protected void onStop() {
         super.onStop();
-        Log.d(LOGGER_TAG, "onStop() called");
+        Log.d( LOGGER_TAG, "onStop() called" );
     }
 
     @Override
@@ -261,10 +215,21 @@ public class TimelineActivity extends PreferencesEnabledActivity implements OnCl
     @Override
     public boolean onContextItemSelected( MenuItem item ) {
         AdapterContextMenuInfo info = ( AdapterContextMenuInfo ) item.getMenuInfo();
-        YambaPost status = ( YambaPost ) view.getItemAtPosition( info.position );
+
+        Cursor cursor = ( Cursor ) view.getItemAtPosition( info.position );
+
+        // YambaPost status = ( YambaPost ) view.getItemAtPosition( info.position );
+
+        long id = cursor.getLong( cursor.getColumnIndex( TweetContract._ID ) );
+        String screenName = cursor.getString( cursor.getColumnIndex( TweetContract.USER ) );
+        Date tweetDate = new Date( cursor.getLong( cursor.getColumnIndex( TweetContract.TIMESTAMP ) ) );
+        String tweet = cursor.getString( cursor.getColumnIndex( TweetContract.TWEET ) );
+
+        YambaPost status = new YambaPost( id, screenName, tweetDate, tweet );
 
         if ( item.getItemId() == R.id.timelineemailctxmenu_sendemail ) {
             Intent it = new Intent( Intent.ACTION_SEND );
+            
             it.setType( "message/rfc822" );
             it.putExtra( Intent.EXTRA_SUBJECT, getString( R.string.timelineitem_emailsubject ) );
             it.putExtra( Intent.EXTRA_TEXT, status.getDataForEmail() );
@@ -277,28 +242,20 @@ public class TimelineActivity extends PreferencesEnabledActivity implements OnCl
         Intent intent = new Intent( this, DetailActivity.class );
         intent.addFlags( Intent.FLAG_ACTIVITY_SINGLE_TOP );
 
-        YambaPost status = timeline.get( position );
+        Cursor cursor = ( Cursor ) parent.getItemAtPosition( position );
+
+        long _id = cursor.getLong( cursor.getColumnIndex( TweetContract._ID ) );
+        String screenName = cursor.getString( cursor.getColumnIndex( TweetContract.USER ) );
+        Date tweetDate = new Date( cursor.getLong( cursor.getColumnIndex( TweetContract.TIMESTAMP ) ) );
+        String tweet = cursor.getString( cursor.getColumnIndex( TweetContract.TWEET ) );
+        
+        YambaPost status = new YambaPost( _id, screenName, tweetDate, tweet );
+
+        // YambaPost status = timeline.get( position );
         if ( status != null ) {
             intent.putExtra( YambaPost.IDENTIFIER, status );
             startActivity( intent );
         }
     }
-//
-//    public void onYambaTimelineChange() {
-//        YambaPDMApplication app = ( YambaPDMApplication ) getApplication();
-//        List< Twitter.Status > currentTimeline = app.getCurrentTimeline();
-//
-//        for ( Twitter.Status status : currentTimeline ) {
-//            Twitter.User twitterUser = status.getUser();
-//
-//            YambaUser user = new YambaUser( twitterUser.getId(), twitterUser.getName(), twitterUser.getFriendsCount(),
-//                    twitterUser.getFollowersCount(), twitterUser.getStatusesCount() );
-//
-//            YambaPost temp = new YambaPost( status.getId(), user, status.getCreatedAt(), status.getText() );
-//            timeline.add( temp );
-//        }
-//
-//        adapter.notifyDataSetChanged();
-//        enableRefresh();
-//    }
+    
 }
